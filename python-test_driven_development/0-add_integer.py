f4ceee923a73c86fb276ca719c474a753c561c4e
CaloMorell <oscarmorell07@yahoo.com>
2023-10-13 14:37:03 +0000

    0. Integers addition

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Este módulo contiene una función que devuelve
+     la suma de dos números enteros.
+"""
+
+
+def add_integer(a, b=98):
+    """Suma dos números enteros.
+
+     Argumentos:
+         a (int): el primer argumento debe ser un número entero o flotante.
+         b (int): el segundo argumento debe ser un número entero o flotante.
+                     valor predeterminado: 98
+     Sube:
+         TypeError: var debe ser un número entero.
+         TypeError: var debe ser un número entero.
+
+     Devoluciones:
+         int: devuelve la suma de a y b.
+     """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
