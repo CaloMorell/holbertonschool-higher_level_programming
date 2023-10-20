bb3b80ba8b85af21068a2c676975388414166c08
CaloMorell <oscarmorell07@yahoo.com>
2023-10-20 14:08:49 +0000

    1. My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+!#/usr/bin/python3
+"""module for MyList class."""
+
+
+class Mylist(list):
+    """Custom MyList class."""
+    def print_sorted(self):
+        """Method for printing sorted list."""
+        print(sorted(self))
