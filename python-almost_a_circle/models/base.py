0039ffc91fe0bfd89933a914017aacad7dc9a3f9
CaloMorell <oscarmorell07@yahoo.com>
2023-10-23 19:13:46 +0000

    1. Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,8 +5,7 @@ from os import path
 
 
 class Base:
-
-
+    """Class Base that will be the parent all the classes in the project"""
     __nb_objects = 0
 
 
@@ -16,22 +15,22 @@ class Base:
             self.id = id
         else:
             Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+            self.id = Base.__nb_objects
 
 
 
     @staticmethod
     def to_json_string(list_dictionaries):
-
+        """Returns a JSON string representation of list_dictionaries"""
         if list_dictionaries is None or not list_dictionaries:
-            return "[]"
+            return '[]'
         return json.dumps(list_dictionaries)
 
 
 
-    @classmethod
+   @classmethod
     def save_to_file(cls, list_objs):
-
+        """Writes the JSON string representation of list_objs to a file"""
         flname = cls.__name__ + '.json'
         dic = []
         if list_objs is not None:
@@ -40,17 +39,17 @@ class Base:
             f.write(cls.to_json_string(dic))
 
 
-    @staticmethod
+  @staticmethod
     def from_json_string(json_string):
-
+        """Returns the list of the JSON string representation json_string"""
         if json_string is None or not json_string:
             return []
         return json.loads(json_string)
 
 
-    @classmethod
+   @classmethod
     def create(cls, **dictionary):
-
+        """Returns an instance with all attributes already set"""
         if cls.__name__ == 'Square':
             new_base = cls(1)
         else:
@@ -61,7 +60,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-
+        """Returns a list of instances"""
         flname = cls.__name__ + '.json'
         if path.isfile(flname):
             with open(flname, 'r') as f:
