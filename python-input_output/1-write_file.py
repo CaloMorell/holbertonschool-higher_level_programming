7a0909f9890bd2e10570df54e6d9330c7599c184
CaloMorell <oscarmorell07@yahoo.com>
2023-10-23 09:25:31 +0000

    1. Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" contains the number_of_lines function """
+"""writes a string to a text file (UTF8)
+"""
 
 
-def number_of_lines(filename=""):
-    """ returns the number of lines in a file """
-    count = 0
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            count += 1
-    return count
+def write_file(filename="", text=""):
+    """returns the number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as myFile:
+        return myFile.write(text)
