2e9debf95fa79055e6b5ed778c75990c8ccb5b58
CaloMorell <oscarmorell07@yahoo.com>
2023-10-23 13:29:44 +0000

    2. First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""The class Rectangle that inherits from Base:"""
+
+
+from models.Base import Base
+
+
+class Rectangular(Base):
+    """Create a rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None)
+    """class Contructor"""
+
+
+    """private attributes"""
+    self.width = width
+    self.height = height
+    self.x = x
+    self.y = y
+    super().__init__(id)
