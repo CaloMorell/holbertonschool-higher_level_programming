a57e25f560193bd85375f79f07a493412c400f6f
CaloMorell <oscarmorell07@yahoo.com>
2023-11-21 08:05:28 -0400

    DROP DATABASE IF EXISTS

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
   -- Delete a database: `hbtn_0c_0`
-  -- If the database `hbtn_0c_0` doesnt exist, script should not fail
+  -- If the database `hbtn_0c_0` doesn't exist, script should not fail
   DROP DATABASE IF EXISTS `hbtn_0c_0`;
