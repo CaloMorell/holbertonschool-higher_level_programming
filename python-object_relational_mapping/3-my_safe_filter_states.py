cb9b048158c898328687f96829fbc725f156e162
CaloMorell <oscarmorell07@yahoo.com>
2023-11-24 22:52:29 -0400

    SQL Injection...

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    # Obtiene los detalles de la conexión MySQL y el nombre del estado desde los argumentos de la línea de comandos
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+    name = argv[4]
+
+    # Establece una conexión con la base de datos MySQL
+    db = MySQLdb.connect(host='localhost', port=3306, user=user, passwd=password, db=database)
+
+    # Crea un objeto cursor para ejecutar consultas SQL
+    cur = db.cursor()
+
+    # Utiliza una consulta parametrizada para evitar la inyección SQL
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (name,))
+
+    # Obtiene todas las filas devueltas por la consulta SQL
+    rows = cur.fetchall()
+
+    # Imprime los resultados
+    for row in rows:
+        print(row)
+
+    # Cierra el cursor para liberar recursos
+    cur.close()
+
+    # Cierra la conexión a la base de datos
+    db.close()
