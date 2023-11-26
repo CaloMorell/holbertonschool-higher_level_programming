7c9e8f77dc890462448f8e4fff86d69757109efc
CaloMorell <oscarmorell07@yahoo.com>
2023-11-26 19:36:59 -0400

    Contains `a` update

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
 """
-Script que lista todos los objetos State de la base de datos que contienen la letra 'a'
+Script that lists all State objects from the database that contain letter a
 """
 
 from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    # Obtiene los detalles de conexión MySQL desde los argumentos de la línea de comandos
+
     user = argv[1]
     password = argv[2]
     database = argv[3]
 
-    # Crea un motor SQLAlchemy
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format (user, password, database), pool_pre_ping=True)
-    
-    # Crea las tablas si no existen
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format
+        (user, password, database), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    # Crea una sesión
     session = Session(engine)
-
-    # Itera sobre todos los objetos State y los imprime si contienen la letra 'a'
     for state in session.query(State).order_by(State.id.asc()).all():
         if 'a' in state.name:
             print("{}: {}".format(state.id, state.name))
-
-    # Cierra la sesión
     session.close()
