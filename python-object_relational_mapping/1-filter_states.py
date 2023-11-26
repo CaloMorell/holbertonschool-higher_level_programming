42ebba31bacbf79e5398acaee32344ac924a36fb
CaloMorell <oscarmorell07@yahoo.com>
2023-11-26 18:29:27 -0400

    Filter states update

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,31 +7,38 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    # Verifica si se proporciona el número correcto de argumentos de línea de comandos
+    """Verifica si se proporciona el número correcto de
+    argumentos de línea de comandos"""
     if len(argv) == 4:
-        # Obtiene los detalles de la conexión MySQL desde los argumentos de la línea de comandos
+        """Obtiene los detalles de la conexión MySQL
+        desde los argumentos de la línea de comandos"""
         user = argv[1]
         password = argv[2]
         database = argv[3]
 
-        # Establece una conexión con la base de datos MySQL
-        db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=password, db=database)
+        """Establece una conexión con la
+        base de datos MySQL"""
+        db = MySQLdb.connect(
+            host="localhost", port=3306, user=user,
+            passwd=password, db=database)
 
-        # Crea un objeto cursor para ejecutar consultas SQL
+        """Crea un objeto cursor para ejecutar consultas SQL"""
         cur = db.cursor()
 
-        # Ejecuta una consulta SQL para seleccionar estados cuyos nombres comienzan con 'N' (mayúscula)
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        """Ejecuta una consulta SQL para seleccionar estados
+        cuyos nombres comienzan con 'N' (mayúscula)"""
+        cur.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-        # Obtiene todas las filas devueltas por la consulta SQL
+        """Obtiene todas las filas devueltas por la consulta SQL"""
         rows = cur.fetchall()
 
-        # Imprime los resultados
+        """Imprime los resultados"""
         for row in rows:
             print(row)
 
-        # Cierra el cursor para liberar recursos
+        """Cierra el cursor para liberar recursos"""
         cur.close()
 
-        # Cierra la conexión a la base de datos
+        """Cierra la conexión a la base de datos"""
         db.close()
