ba0cf36c0e8d6605cef128d31f366a1d3b9712b0
CaloMorell <oscarmorell07@yahoo.com>
2023-11-26 19:32:32 -0400

    All cities by state  update

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,41 +1,32 @@
 #!/usr/bin/python3
 """
-Script lists all cities from the database in ascending order by id
+Script lists all cities from the database that are in a specified state
 """
-
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    # Obtiene los detalles de la conexión MySQL y el nombre del estado desde los argumentos de la línea de comandos
+
     user = argv[1]
     password = argv[2]
     database = argv[3]
     state = argv[4]
-    cities = []  # Inicializa una lista vacía para almacenar los nombres de las ciudades
+    cities = []
 
-    # Establece una conexión con la base de datos MySQL
-    db = MySQLdb.connect(host='localhost', port=3306, user=user, passwd=password, db=database)
+    db = MySQLdb.connect(
+        host='localhost', port=3306, user=user,
+        passwd=password, db=database)
 
-    # Crea un objeto cursor para ejecutar consultas SQL
     cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name
+    FROM cities INNER JOIN states ON cities.state_id = states.id
+    WHERE states.name=%s
+    ORDER BY id ASC""", (state,))
 
-    # Ejecuta una consulta SQL para obtener información de las ciudades desde las tablas 'cities' y 'states' para el estado especificado
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name=%s ORDER BY id ASC""", (state,))
-
-    # Obtiene todas las filas devueltas por la consulta SQL
     rows = cur.fetchall()
-
-    # Añade los nombres de las ciudades a la lista 'cities'
     for row in rows:
         cities.append(row[1])
-
-    # Imprime la lista de nombres de ciudades, separados por comas
     print(', '.join(cities))
-
-    # Cierra el cursor para liberar recursos
     cur.close()
-
-    # Cierra la conexión a la base de datos
     db.close()
