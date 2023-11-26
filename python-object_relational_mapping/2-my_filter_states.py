f24175206dfe6f9c08a8758be23433f95dc51b55
CaloMorell <oscarmorell07@yahoo.com>
2023-11-26 19:26:05 -0400

    Filter states by user input update

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,35 +7,22 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
+
     user = argv[1]
     password = argv[2]
     database = argv[3]
     name = argv[4]
+    quer = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(name)
 
-    # Establece una conexión con la base de datos MySQL
     db = MySQLdb.connect(
-        host="localhost", port=3306, user=user,
+        host='localhost', port=3306, user=user,
         passwd=password, db=database)
-
-    # Crea un objeto cursor para ejecutar consultas SQL
     cur = db.cursor()
+    cur.execute(quer)
 
-    # Construye la consulta SQL con el nombre proporcionado por el usuario
-    query =
-    "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(name)
-
-    # Ejecuta la consulta SQL
-    cur.execute(query)
-
-    # Obtiene todas las filas devueltas por la consulta SQL
     rows = cur.fetchall()
-
-    # Imprime los resultados
     for row in rows:
-        print(row)
-
-    # Cierra el cursor para liberar recursos
+        if row[1] == name:
+            print(row)
     cur.close()
-
-    # Cierra la conexión a la base de datos
     db.close()
