5e32756bc6c7551592f75b6d5ed4e5872836f548
CaloMorell <oscarmorell07@yahoo.com>
2023-11-26 19:35:50 -0400

    First state update

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,37 +1,30 @@
 #!/usr/bin/python3
-
 """
-Script que imprime el primer objeto State de la base de datos
+Script prints the first State object from the database
 """
 
 from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    # Obtiene los detalles de conexión MySQL desde los argumentos de la línea de comandos
+
     user = argv[1]
     password = argv[2]
     database = argv[3]
 
-    # Crea un motor SQLAlchemy
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format (user, password, database), pool_pre_ping=True)
-    
-    # Crea las tablas si no existen
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format
+        (user, password, database), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    # Crea una sesión
     session = Session(engine)
-
-    # Consulta el primer objeto State
     state = session.query(State).first()
 
-    # Imprime el ID y el nombre del primer State si existe, de lo contrario, imprime "Nothing"
     if state is not None:
         print("{}: {}".format(state.id, state.name))
     else:
         print("Nothing")
 
-    # Cierra la sesión
     session.close()
